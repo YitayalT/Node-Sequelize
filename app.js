eafacf64478d4ddb2b1a64d90c6b59f4890a2047
YitayalT <anex518@gmail.com>
2022-07-04 10:46:21 +0300

    commit

--- app.js
+++ app.js
@@ -73,54 +73,7 @@ app.use(express.json());
 //      console.log(err)
 //     });
 
-// model relationships
-// Client.hasMany(PreventiveCare, {
-//   foreignKey: 'MRN'
-// });
 
-// User.hasMany(PreventiveCare, {
-//   foreignKey: 'UserId'
-// });
-
-// Client.hasMany(ClassifyingForm, {
-//   foreignKey: 'MRN'
-// });
-
-// User.hasMany(ClassifyingForm, {
-//   foreignKey: 'UserId'
-// })
-
-Client.hasMany(AncVisit, {
-  foreignKey: 'MRN'
-});
-
-AncVisit.belongsTo(Client, {
-  foreignKey: "MRN",
-});
-User.hasMany(AncVisit, {
-  foreignKey: 'UserId'
-})
-AncVisit.belongsTo(User, {
-  foreignKey: "UserId",
-});
-
-// Client.hasMany(Prescription, {
-//   foreignKey: 'MRN'
-// });
- 
-// Prescription.belongsTo(Client, {
-//   foreignKey: "MRN",
-// });
-// User.hasMany(Prescription, {
-//   foreignKey: 'UserId'
-// })
-// Prescription.belongsTo(User, {
-//   foreignKey: "UserId",
-// });
-
-// User.hasMany(Client, {
-//   foreignKey: 'UserId'
-// })
 
 //  AncVisit.sync({force: true}).then( () =>{
 //     console.log('synced!');
---
+++ config/relationship.js
@@ -0,0 +1,48 @@
+// model relationships
+// Client.hasMany(PreventiveCare, {
+//   foreignKey: 'MRN'
+// });
+
+// User.hasMany(PreventiveCare, {
+//   foreignKey: 'UserId'
+// });
+
+// Client.hasMany(ClassifyingForm, {
+//   foreignKey: 'MRN'
+// });
+
+// User.hasMany(ClassifyingForm, {
+//   foreignKey: 'UserId'
+// })
+
+// Client.hasMany(AncVisit, {
+//   foreignKey: "MRN",
+// });
+
+// AncVisit.belongsTo(Client, {
+//   foreignKey: "MRN",
+// });
+// User.hasMany(AncVisit, {
+//   foreignKey: "UserId",
+// });
+// AncVisit.belongsTo(User, {
+//   foreignKey: "UserId",
+// });
+
+// Client.hasMany(Prescription, {
+//   foreignKey: 'MRN'
+// });
+
+// Prescription.belongsTo(Client, {
+//   foreignKey: "MRN",
+// });
+// User.hasMany(Prescription, {
+//   foreignKey: 'UserId'
+// })
+// Prescription.belongsTo(User, {
+//   foreignKey: "UserId",
+// });
+
+// User.hasMany(Client, {
+//   foreignKey: 'UserId'
+// })
